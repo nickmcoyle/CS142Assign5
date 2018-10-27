f42deeeac68f070c59ea779fb3afac28bc12acba
nickmcoyle <nickmcoyle@gmail.com>
2018-10-27 01:26:44 -0700

    added all methods

--- Time.java
+++ Time.java
@@ -7,5 +7,61 @@
  */
 public class Time
 {
+    private int minute;
+    private int hour;
+    
+    Time() {
+        this(0,0);
+    }
+    
+    Time(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }    
+
+    /*
+     * method to calculate the amount of hour and minutes difference between two times
+     * 
+     * @param Time other is the Time object that we will be comparing the current Time object to
+     */
+    public Time timeBetween(Time other) {
+        return new Time();
+    }
+
+    /*
+     * method to format the hour or minute component of a Time object to make it display better
+     * 
+     * @param int timePart is the hour or minute we will format
+     */
+    public String zeroTime(int timePart) {
+        String timeString =  Integer.toString(timePart);
+        
+        if(timePart >= 0 && timePart <= 9) {
+            timeString += "0" + timePart;
+        }
+        
+        return timeString;
+    }
+
+    @Override
+    public String toString() {
+        return new String(zeroTime(hour) + ":" + zeroTime(minute));
+    }
 
 }
--- Train.java
+++ Train.java
@@ -7,5 +7,74 @@
  */
 public class Train
 {
- 
+    private String name;    
+    private Time departure;
+    private Time arrival;
+    private int distance;
+
+    Train() {
+        this("", new Time(), new Time(), 0);
+    }
+
+    Train(String name, Time departure, Time arrival, int distance) {
+        this.name = name;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.distance = distance;
+    }
+
+    public Time getDeparture () {
+        return departure;
+    }
+
+    public Time getArrival () {
+        return arrival;
+    }
+
+    public int getDistance () {
+        return distance;
+    }
+
+    public void setDeparture(Time departure) {
+        this.departure = departure;   
+    }
+
+    public void setArrival(Time arrival) {
+        this.arrival = arrival;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    /**
+     * method to calculate the average speed a Train travelled using the distance and total time of travel
+     * this is calculated using the formula distance = rate*time
+     */
+    public int averageSpeed() {
+        //distance = rate * time
+        //rate = distance / time;
+        Time travelTime = travelTime();
+        double hours = travelTime.getHour() + (double)travelTime.getMinute()/60;
+        return (int)(distance / hours);
+    }
+
+    /**
+     * method to calculate the amount of time the Train travelled
+     * this is calculated using the timeBetween() method of the Time class
+     */
+    public Time travelTime() {        
+        return departure.timeBetween(arrival);
+    }
+
+    @Override
+    public String toString() {
+        String str = name + "\n";
+        str += "Departure             " + departure + "\n";
+        str += "Arrival               " + arrival + "\n";
+        str += "Travel Time           " + travelTime() + "\n";
+        str += "Average Speed         " + averageSpeed() + "km/h" + "\n";
+        
+        return str;
+    }
 }
